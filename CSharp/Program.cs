d44f7dce6b2a58d2179d14ca7321cc457df92cf4
jmmmel <youcantseeme.iamninja@gmail.com>
2015-11-06 09:30:32 -0700

    Adding MergeSort code

---
+++ CSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+
+
+namespace Inquisitiveness
+{
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
+
+            int len = 9;
+
+            
+            Console.WriteLine("MergeSort By Recursive Method");
+
+            MergeSort.MergeSort_Recursive(numbers, 0, len - 1);
+
+            for (int i = 0; i < 9; i++)
+
+                Console.WriteLine(numbers[i]);
+
+
+
+        }
+
+    }
+}
