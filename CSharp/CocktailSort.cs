f46e31659893feb5bd87fa9b45cf546fe5698ffb
jmmmel <youcantseeme.iamninja@gmail.com>
2015-11-11 07:57:51 -0700

    All C# code

---
+++ CSharp/CocktailSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inquisitiveness
+{
+    class CocktailSort
+    {
+        public static void Sort(ref double[] x)
+        {
+            for (int k = x.Length - 1; k > 0; k--)
+            {
+                bool swapped = false;
+                for (int i = k; i > 0; i--)
+                    if (x[i] < x[i - 1])
+                    {
+                        // swap
+                        double temp = x[i];
+                        x[i] = x[i - 1];
+                        x[i - 1] = temp;
+                        swapped = true;
+                    }
+
+                for (int i = 0; i < k; i++)
+                    if (x[i] > x[i + 1])
+                    {
+                        // swap
+                        double temp = x[i];
+                        x[i] = x[i + 1];
+                        x[i + 1] = temp;
+                        swapped = true;
+                    }
+
+                if (!swapped)
+                    break;
+            }
+        }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 using System.Text;
 
+using System.Diagnostics;
 
 
 namespace Inquisitiveness
@@ -14,20 +15,66 @@ namespace Inquisitiveness
         
         static void Main(string[] args)
         {
+            Console.Write("\n\nEnter the total number of elements: ");
 
-            int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
+            int max = Convert.ToInt32(Console.ReadLine());
 
-            int len = 9;
 
-            
-            Console.WriteLine("MergeSort By Recursive Method");
 
-            MergeSort.MergeSort_Recursive(numbers, 0, len - 1);
+            double[] mergeArray = new double[max];
+          //  double[] bubbleArray = new double[max];
+          //  double[] cocktailArray = new double[max];
+            double[] quickArray = new double[max];
 
-            for (int i = 0; i < 9; i++)
 
-                Console.WriteLine(numbers[i]);
+            for (int i = 0; i < max; i++)
+            {
+                mergeArray[i] = i;
+               // bubbleArray[i] = i;
+             //   cocktailArray[i] = i;
+                quickArray[i] = i;
+            }
+            Random rnd = new Random();
+            for (int i = 0; i < max; i++)
+            {
 
+                double swap = mergeArray[i];
+                int randIndex = rnd.Next(0, (max - 1));
+                mergeArray[i] = mergeArray[randIndex];
+                mergeArray[randIndex] = swap;
+                /*
+                swap = bubbleArray[i];
+                bubbleArray[i] = bubbleArray[randIndex];
+                bubbleArray[randIndex] = swap;
+
+                swap = cocktailArray[i];
+                cocktailArray[i] = cocktailArray[randIndex];
+                cocktailArray[randIndex] = swap;
+                */
+                swap = quickArray[i];
+                quickArray[i] = quickArray[randIndex];
+                quickArray[randIndex] = swap;
+
+            }
+
+            //Bubble.BubbleSort(max, bubbleArray);
+
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            //CocktailSort.Sort(ref cocktailArray);
+            stopWatch.Stop();
+            Console.WriteLine("TIME TAKEN in COCKTAIL SORT: " + stopWatch.Elapsed);
+
+
+            stopWatch = Stopwatch.StartNew();
+            MergeSort.MergeSort_Recursive(mergeArray, 0, (max - 1));
+            stopWatch.Stop();
+            Console.WriteLine("TIME TAKEN in MERGE SORT: " + stopWatch.Elapsed);
+
+            stopWatch = Stopwatch.StartNew();
+            QuickSort.Sort(quickArray, max);
+            stopWatch.Stop();
+            Console.WriteLine("TIME TAKEN in QUICK SORT: " + stopWatch.Elapsed);
 
 
         }
