d706b73ac2cef7a51a09391620c434ce438b84ba
Nghia (Jason) Vuong <vuo13001@byui.edu>
2015-12-08 03:38:07 -0700

    Update vastnessZZ.cpp

--- vastnessZZ.cpp
+++ vastnessZZ.cpp
@@ -5,11 +5,10 @@
 #include <NTL/ZZ.h>
 using namespace std;
 NTL_CLIENT
-
 /************************
-* Compile with:   
-*   g++ vastnessZZ.cpp -lntl
-*************************/
+ * Compile with:   
+ *   g++ vastnessZZ.cpp -lntl
+ *************************/
 
 bool check = false;
 long baseTwo = 2;
@@ -28,24 +27,25 @@ int main ()
    cout << "Enter the prefix: ";
    cin >> prefix;
    assert(prefix >= 1);
+   
    while (check == false)
    {
-	  power(expo, baseTen, n);
+      power(expo, baseTen, n);
       num = (prefix + to_ZZ(1)) * expo;
       numLog = floor(log(num) / log(baseTwo));
       power(Test, baseTwo, numLog);
       newTest = Test / expo;
       
-	  if (prefix == newTest)
+      if (prefix == newTest)
       {
-	     if (prefix == 1)
-	     {
-		    cout << "Prefix: 1 | 2^0 = 1" << endl;
-		    return 0;
-	     }
+         if (prefix == 1)
+         {
+            cout << "Prefix: 1 | 2^0 = 1" << endl;
+            return 0;
+         }
          cout << "Prefix: " << prefix    << " | "
-		          << "2^"       << numLog
-				<< " = "       << Test     << endl;
+              << "2^"       << numLog
+              << " = "      << Test      << endl;
          check = true;
       }
       n++;
